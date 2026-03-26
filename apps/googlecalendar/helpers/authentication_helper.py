d7c6901fe27d5d32925c04e903c6dc33701e0da2
barack-joemama <phamducloc2003@gmail.com>
2026-03-26 01:13:22 +0100

    Added settings to gitignore

--- apps/googlecalendar/helpers/authentication_helper.py
+++ apps/googlecalendar/helpers/authentication_helper.py
@@ -1,17 +1,14 @@
 import logging
-import os
 
 from django.conf import settings
 from google_auth_oauthlib.flow import Flow
 
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
-CREDENTIALS_FILE = os.path.join(settings.BASE_DIR, "credentials.json")
 logger = logging.getLogger(__name__)
 
 
 def get_flow(redirect_uri):
     """Create the OAuth flow instance."""
     flow = Flow.from_client_secrets_file(
-        CREDENTIALS_FILE, scopes=SCOPES, redirect_uri=redirect_uri
+        settings.GOOGLE_CREDENTIALS_PATH, scopes=settings.GOOGLE_SCOPES, redirect_uri=redirect_uri
     )
     return flow
