120035251f26d6635d921492ef0eb89094f459a7
barack-joemama <phamducloc2003@gmail.com>
2026-04-12 02:49:00 +0200

    Fix missing name and rearrange de code

--- apps/spotify/helpers/playback_helper.py
+++ apps/spotify/helpers/playback_helper.py
@@ -22,16 +22,18 @@ def get_playback(access_token):
                 "height": playback_item.get("album").get("images")[0].get("height"),
             },
         }
+        repeat_state = playback_object.get("repeat_state")
         progress_ms = playback_object.get("progress_ms")
         duration_ms = playback_item.get("duration_ms")
         progress = progress_ms / duration_ms
-        track_name = playback_object.get("name")
+        track_name = playback_item.get("name")
         track_id = playback_item.get("id")
         artists = []
         for artist_object in playback_item.get("artists"):
             artists.append(artist_object.get("name"))
         return {
             "currently_playing": {
+                "repeat_state": repeat_state,
                 "track_id": track_id,
                 "track_name": track_name,
                 "duration_ms": duration_ms,
@@ -67,5 +69,7 @@ def get_stream_data(access_token):
                 "track_id": track_id,
             },
         }
-    else:
-        return {"currently_playing": None}
+    elif response.status_code == 401:
+        return {"error": "Token expired, please login again"}
+    elif response.status_code == 204:
+        return {"error": "Playback is currently unavailable. Play something on spotify!"}
--- apps/spotify/views/views.py
+++ apps/spotify/views/views.py
@@ -16,6 +16,7 @@ from apps.spotify.constants import scope, curr_playback_key
 
 redirect_uri = os.environ["SPOTIFY_REDIRECT_URI"]
 client_id = os.environ["SPOTIFY_CLIENT_ID"]
+client_secret = os.environ["SPOTIFY_CLIENT_SECRET"]
 
 load_dotenv()
 
@@ -32,7 +33,7 @@ def index(request):
     return render(request, "spotify/index.html")
 
 
-def login():
+def login(request):
     state = str(uuid.uuid4())
 
     response_type = "code"
@@ -54,8 +55,6 @@ def callback(request):
 
     if state is None:
         return redirect("/")
-
-    client_secret = os.environ["SPOTIFY_CLIENT_SECRET"]
 
     credentials = base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
 
@@ -96,18 +95,18 @@ def track_progress_stream(request):
             access_token = request.session["spotify_credentials"]
             stream_data = get_stream_data(access_token)
 
-            if stream_data.get("currently_playing") is not None:
+            if "currently_playing" in stream_data:
                 if not cache.get(curr_playback_key):
                     cache.set(curr_playback_key, get_playback(access_token))
-
-                curr_playback_data = cache.get(curr_playback_key)
-                if curr_playback_data.get("track_id") != stream_data.get("track_id"):
-                    cache.set(curr_playback_key, get_playback(access_token))
+                else:
+                    curr_playback_data = cache.get(curr_playback_key)
+                    if curr_playback_data.get("track_id") != stream_data.get("track_id"):
+                        cache.set(curr_playback_key, get_playback(access_token))
 
                 yield json.dumps(stream_data) + "\n"
             else:
                 yield (
-                    json.dumps({"status": "No track is playing, start one on Spotify!"})
+                    json.dumps(stream_data)
                     + "\n"
                 )
 
